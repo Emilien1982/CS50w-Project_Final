a34c2d8c27c0de153a392deb79b8c602003305ef
Emilien Dre <emilien.dreyer@gmail.com>
2021-03-26 18:22:07 +0100

    secret key changed and hide

--- project_final/booking/views.py
+++ project_final/booking/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from datetime import datetime, timedelta
+
+
 
 # Create your views here.
+def index(request):
+    today = datetime.today()
+    booking_end = today + timedelta(days=62)
+    print(booking_end.strftime('%Y-%m-%d'))
+    return render(request, "booking/index.html", {
+        "today": today.strftime('%Y-%m-%d'),
+        "booking_end": booking_end.strftime('%Y-%m-%d'),
+    })
--- project_final/project_final/settings.py
+++ project_final/project_final/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0bp118ucwki-ecj$v7)^dddo@d8^ou&j70sr=k#mo(1as9*zv3'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
